4b954f191d486fa4be8e20eee054624a10ab87e4
carlosgnotario <dlacrem.info@gmail.com>
2025-06-11 02:56:59 +0200

    changes to accordion tabs

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
-const accordion = document.querySelectorAll(".accordion");
+const tabsSystem = document.querySelectorAll("#tabify");
+console.log(tabsSystem);
 
-accordion.forEach((el, i) => {
-    const accordionItem = el.querySelectorAll(".accordion-item");
+tabsSystem.forEach((el, i) => {
+    const tabsLinks = el.querySelectorAll(".w-tab-link");
+    const tabsContentItems = el.querySelectorAll(".w-tab-pane");
 
-    accordionItem.forEach((item, i) => {
-        item.addEventListener("click", () => {
-            accordionItem.forEach((item, i) => {
-                item.classList.remove("active");
-            });
-            item.classList.add("active");
+    tabsLinks.forEach((link, i) => { 
+        link.style.order = i;
+
+        link.addEventListener("click", () => {
+            // scroll to link
+            setTimeout(() => {
+                link.scrollIntoView({ alignToTop: true, behavior: "smooth" });
+                console.log("clicked");
+            }, 1000);
         });
-    });
+    });    
+    tabsContentItems.forEach((item, i) => { item.style.order = i; });
 });
