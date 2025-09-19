3ccd5f248a0c63eb96f0051318ddcf7d854001cb
Katia <hello@katiasmet.com>
2025-09-19 10:35:49 +0200

    feat: speed up

--- js/classes/VideoBlock.js
+++ js/classes/VideoBlock.js
@@ -1,70 +1,72 @@
-import gsap from 'gsap';
+import gsap from "gsap";
 
 export default class VideoBlock {
-  static selector = '.carousel-video';
-  
+  static selector = ".carousel-video";
+
   constructor(element, options = {}) {
     this.element = element;
     this.options = options;
-    
+
     this.elements();
     this.setup();
     this.interactions();
   }
-  
+
   elements() {
-    this.button = this.element.querySelector('.liquid-button');
-    this.testimonial = this.element.querySelector('.carousel-video-testimonial');
-    this.bg = this.element.querySelector('.carousel-background');
-    this.img = this.element.querySelector('.carousel-thumbnail');
+    this.button = this.element.querySelector(".liquid-button");
+    this.testimonial = this.element.querySelector(
+      ".carousel-video-testimonial"
+    );
+    this.bg = this.element.querySelector(".carousel-background");
+    this.img = this.element.querySelector(".carousel-thumbnail");
   }
 
   setup() {
     gsap.set(this.button, {
-        opacity: 0,
-    })
+      opacity: 0,
+    });
     gsap.set(this.img, {
-      scale: 1.2
-    })
+      scale: 1.2,
+    });
   }
 
   interactions() {
-    this.element.addEventListener('mouseenter', () => {
-        this.animateIn();
-    })
-    this.element.addEventListener('mouseleave', () => {
-        this.animateOut();
-    })
+    this.element.addEventListener("mouseenter", () => {
+      this.animateIn();
+    });
+    this.element.addEventListener("mouseleave", () => {
+      this.animateOut();
+    });
   }
 
   animateIn() {
     gsap.to(this.button, {
-        opacity: 1,
-        duration: 1,
-        ease: "expo.in"
-    })
+      opacity: 1,
+      duration: 0.5,
+      ease: "expo.in",
+    });
     gsap.to(this.img, {
       scale: 1,
       duration: 3,
       delay: 0.2,
-      ease: "expo.out"
-    })
+      ease: "expo.out",
+    });
   }
 
   animateOut() {
     gsap.to(this.button, {
-        opacity: 0,
-        duration: 0.5,
-        delay: 0.2,
-        ease: "expo.out",
-        overwrite: "auto",
-    })
+      opacity: 0,
+      duration: 0.5,
+      delay: 0.2,
+      ease: "expo.out",
+      overwrite: "auto",
+    });
     gsap.killTweensOf(this.img);
     gsap.to(this.img, {
       scale: 1.2,
       duration: 3,
       ease: "expo.out",
       overwrite: "auto",
-    })
+    });
   }
-} 
+}
