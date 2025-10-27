2a86236ac1dc7f9c1d5a3e984ff41d82c6797ece
carlosgnotario <dlacrem.info@gmail.com>
2025-10-27 17:35:19 +0100

    Add conditional loader for Webflow with local/production switching

--- js/classes/VideoBlock.js
+++ js/classes/VideoBlock.js
@@ -1,72 +1,8 @@
-import gsap from "gsap";
-
 export default class VideoBlock {
   static selector = ".carousel-video";
 
   constructor(element, options = {}) {
     this.element = element;
-    this.options = options;
-
-    this.elements();
-    this.setup();
-    this.interactions();
-  }
-
-  elements() {
-    this.button = this.element.querySelector(".liquid-button");
-    this.testimonial = this.element.querySelector(
-      ".carousel-video-testimonial"
-    );
-    this.bg = this.element.querySelector(".carousel-background");
-    this.img = this.element.querySelector(".carousel-thumbnail");
-  }
-
-  setup() {
-    gsap.set(this.button, {
-      opacity: 0,
-    });
-    gsap.set(this.img, {
-      scale: 1.2,
-    });
-  }
-
-  interactions() {
-    this.element.addEventListener("mouseenter", () => {
-      this.animateIn();
-    });
-    this.element.addEventListener("mouseleave", () => {
-      this.animateOut();
-    });
-  }
-
-  animateIn() {
-    gsap.to(this.button, {
-      opacity: 1,
-      duration: 0.5,
-      ease: "expo.in",
-    });
-    gsap.to(this.img, {
-      scale: 1,
-      duration: 3,
-      delay: 0.2,
-      ease: "expo.out",
-    });
-  }
-
-  animateOut() {
-    gsap.to(this.button, {
-      opacity: 0,
-      duration: 0.5,
-      delay: 0.2,
-      ease: "expo.out",
-      overwrite: "auto",
-    });
-    gsap.killTweensOf(this.img);
-    gsap.to(this.img, {
-      scale: 1.2,
-      duration: 3,
-      ease: "expo.out",
-      overwrite: "auto",
-    });
+    console.log(this.element);
   }
 }
--- js/modules/ClassManager.js
+++ js/modules/ClassManager.js
@@ -1,62 +1,13 @@
 // Import classes
 import VideoBlock from '../classes/VideoBlock.js';
-import Swiper from '../classes/Swiper.js';
-import Cards from '../classes/Card.js';
-import FAQ from '../classes/FAQ.js';
-import HeaderScroll from '../classes/HeaderScroll.js';
-import Animations from '../classes/Animations.js';
-import FormValidation from '../classes/FormValidation.js';
-import LottieConverter from '../classes/lottieconverter.js';
-import ScrollSlider from '../classes/ScrollSlider.js';
 
 // Make classes available globally
-window.AppClasses = { VideoBlock, Swiper, Cards, FAQ, HeaderScroll, Animations, FormValidation };
+window.AppClasses = { VideoBlock };
 
 // Initialize classes on DOM elements
 export default function initializeClasses() {
   // VideoBlock on .carousel-video elements
   document.querySelectorAll(VideoBlock.selector).forEach((element, index) => {
     new VideoBlock(element, { index });
-  });
-
-  document.querySelectorAll("[data-scrollslider]").forEach((element, index) => {
-    new ScrollSlider(element, { index });
-  });
-
-  // Swiper on .swiper elements
-  document.querySelectorAll(Swiper.selector).forEach((element, index) => {
-    // timeout if swiper selector dataset type is resources
-    if (element.dataset.swiper === "resources") {
-      setTimeout(() => {
-        new Swiper(element, { index });
-      }, 1000);
-    } else {
-      new Swiper(element, { index });
-    }
-  });
-
-  // Card on .card elements
-  document.querySelectorAll(Cards.selector).forEach((element, index) => {
-    new Cards(element, { index });
-  });
-
-  // FAQ on .faq elements
-  document.querySelectorAll(FAQ.selector).forEach((element, index) => {
-    new FAQ(element, { index });
-  });
-
-  // HeaderScroll on [data-header-scroll] elements
-  document.querySelectorAll(HeaderScroll.selector).forEach((element, index) => {
-    new HeaderScroll(element, { index });
-  });
-
-  // Animations on [data-animation] elements
-  document.querySelectorAll(Animations.selector).forEach((element, index) => {
-    new Animations(element, { index });
-  });
-
-  // FormValidation on forms with data-validate attribute
-  document.querySelectorAll(FormValidation.selector).forEach((element, index) => {
-    new FormValidation(element, { index });
   });
 }
