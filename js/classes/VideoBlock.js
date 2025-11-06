ae1c788ad341862c33f66ec3e39c8963b0ad3b2f
carlosgnotario <dlacrem.info@gmail.com>
2025-11-06 19:10:58 +0100

    Add VideoFAQ component with SVG checkmark styling and update VideoBox/VideoCarousel classes

--- js/classes/VideoBlock.js
+++
@@ -1,8 +0,0 @@
-export default class VideoBlock {
-  static selector = ".carousel-video";
-
-  constructor(element, options = {}) {
-    this.element = element;
-    console.log(this.element);
-  }
-}
--- js/modules/ClassManager.js
+++ js/modules/ClassManager.js
@@ -1,13 +1,24 @@
 // Import classes
-import VideoBlock from '../classes/VideoBlock.js';
+import VideoBox from '../classes/VideoBox.js';
+import VideoCarousel from '../classes/VideoCarousel.js';
+import VideoFAQ from '../classes/VideoFAQ.js';
 
 // Make classes available globally
-window.AppClasses = { VideoBlock };
+window.AppClasses = { VideoBox, VideoCarousel, VideoFAQ };
 
 // Initialize classes on DOM elements
 export default function initializeClasses() {
   // VideoBlock on .carousel-video elements
-  document.querySelectorAll(VideoBlock.selector).forEach((element, index) => {
-    new VideoBlock(element, { index });
+  document.querySelectorAll(VideoBox.selector).forEach((element, index) => {
+    new VideoBox(element, { index });
+  });
+  
+  // VideoCarousel on elements matching selector
+  document.querySelectorAll(VideoCarousel.selector).forEach((element, index) => {
+    new VideoCarousel(element, { index });
+  });
+
+  document.querySelectorAll(VideoFAQ.selector).forEach((element, index) => {
+    new VideoFAQ(element, { index });
   });
 }
